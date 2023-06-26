30f18911397a07c6a7c105228dff3cbb74f00c35
lukadval <ldval19@freeuni.edu.ge>
2023-06-26 23:10:30 +0400

    methods added to stutordb

--- out/production/oop-final-project-stutor/stutor/script.sql
+++ out/production/oop-final-project-stutor/stutor/script.sql
@@ -4,21 +4,9 @@ USE stutor_db;
 
 DROP TABLE IF EXISTS users;
 
-# CREATE TABLE users (
-#     user_id INT AUTO_INCREMENT primary key,
-#     username VARCHAR(64),
-#     role_id INT
-# );
-
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT primary key,
     username VARCHAR(64),
-    first_name VARCHAR(64),
-    last_name VARCHAR(64),
-    phone VARCHAR(64),
-    email VARCHAR(64),
-    location VARCHAR(64),
-    bio TEXT,
     role_id INT
 );
 
@@ -31,17 +19,29 @@ select * from users;
 DROP TABLE IF EXISTS students;
 
 CREATE TABLE students (
+   student_id INT AUTO_INCREMENT primary key,
    user_id INT,
+   username VARCHAR(64),
+   first_name VARCHAR(64),
+   last_name VARCHAR(64),
    rating DECIMAL(4,2),
    rate_number INT,
+   role_id INT,
+   bio TEXT,
    foreign key (user_id) references users(user_id)
 );
 
 
 CREATE TABLE tutors (
+    tutor_id INT AUTO_INCREMENT primary key,
     user_id INT,
+    username VARCHAR(64),
+    first_name VARCHAR(64),
+    last_name VARCHAR(64),
     rating DECIMAL(4,2),
     rate_number INT,
+    role_id INT,
+    bio TEXT,
     foreign key (user_id) references users(user_id)
 );
 
--- stutor/src/main/java/StutorDB.java
+++ stutor/src/main/java/StutorDB.java
@@ -1,9 +1,11 @@
+import jdk.dynalink.linker.LinkerServices;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class StutorDB {
     String username = "root";
@@ -16,19 +18,39 @@ public class StutorDB {
         ds.setUrl("jdbc:mysql://localhost:3306/stutor_db");
         ds.setUsername("root");
         ds.setPassword("apokalips");
-
-
-        try {
-            Connection connection = ds.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from users;");
-            while(resultSet.next()){
-                System.out.println(resultSet.getString("username"));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            Connection connection = ds.getConnection();
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("select * from users;");
+//            while(resultSet.next()){
+//                System.out.println(resultSet.getString("username"));
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
     }
+
+    public void addAccount(Account account) {
+        // TO DO
+    }
+
+    public void removeAccount(){
+        // TO DO
+    }
+
+    // search username and return list of ids
+    public ArrayList<Account> searchIdLike(String username){
+
+        return null;
+    }
+
+
+    // For later
+    public ArrayList<Account> search(int role, ArrayList<String> subject, int minRating, int maxRating, String username){
+
+        return null;
+    }
+
 
 
 
