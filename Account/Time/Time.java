00d0e21e0b47d96a893cd128054788cf09fa0831
kostabutbaia <kbutb21@freeuni.edu.ge>
2023-07-11 01:43:16 +0400

    organize files

---
+++ Account/Time/Time.java
@@ -0,0 +1,17 @@
+public class Time {
+    private int hour;
+    private int minute;
+
+    public Time(int hour, int minute){
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public int getHour(){
+        return this.hour;
+    }
+
+    public int getMinute(){
+        return this.minute;
+    }
+}
